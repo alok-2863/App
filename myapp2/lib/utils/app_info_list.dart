21aab24d7df381e201835f8ba428982488526199
alok-2863 <alokrajharmu@gmail.com>
2023-01-17 12:44:22 +0530

    Ticket Booking App

---
+++ myapp2/lib/utils/app_info_list.dart
@@ -0,0 +1,20 @@
+List <Map<String, dynamic>> hotelList=[
+  {
+    'image':'one.png',
+    'place':'Open Space',
+    'destinstion':'London',
+    'price': 25
+  },
+  {
+    'image':'two.png',
+    'place':'Global Will',
+    'destinstion':'London',
+    'price': 40
+  },
+  {
+    'image':'three.png',
+    'place':'Tallest Building',
+    'destinstion':'Dubai',
+    'price': 68
+  },
+]
